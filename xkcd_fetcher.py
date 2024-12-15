721b73b2be2fdb45c9a6fe27243f8d7ebf312d0d
captain-nemo-10994 <93784270+captain-nemo-10994@users.noreply.github.com>
2024-12-14 23:43:04 -0600

    Update xkcd_fetcher.py
    Switched from matplotlib to pyqt6. Added alt text.

--- xkcd_fetcher.py
+++ xkcd_fetcher.py
@@ -1,21 +1,73 @@
 import requests as req
-import os, bs4, json
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
+import os, json, sys
 
 url = 'https://xkcd.com/'
 
 os.makedirs('xkcd',exist_ok=True)
 
-res = req.get("https://xkcd.com/info.0.json")
-comic_details = json.loads(res.text)
+def get_comic_details():
+    res = req.get("https://xkcd.com/info.0.json")
+    return json.loads(res.text)
 
-print(comic_details.get("img"))
-res = req.get(comic_details.get("img"))
+def get_comic(url, title):
+    res = req.get(url).content
 
-plt.xticks([])  
-plt.yticks([])
-plt.xlabel("\n" + comic_details.get("alt"), wrap=True)
-img = mpimg.imread('xkcd/metar.png')
-imgplot = plt.imshow(img)
-plt.show()
+    with open(title + ".png", 'wb') as handler:
+        handler.write(res)
+
+class MainWindow(QMainWindow):
+    def __init__(self, x, y, alt, title):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        self.x = x
+        self.y = y
+        self.alt = alt
+        self.title = title
+
+        comic = QLabel(self)
+        pixmap = QPixmap(self.title + ".png")
+        comic.setPixmap(pixmap)
+        comic.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        alt_label = QLabel(alt,self)
+        font = alt_label.font()
+        font.setPointSize(12)
+        alt_label.setFont(font)
+        alt_label.setWordWrap(True)
+        alt_label.setContentsMargins(10,15,10,10)
+        alt_label.setAlignment(Qt.AlignmentFlag.AlignBottom)
+        
+        comic.adjustSize()
+        alt_label.adjustSize()
+        comic.setFixedSize(comic.size())
+        
+        layout.addWidget(comic)
+        layout.addWidget(alt_label)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+        self.resize(self.minimumSizeHint())
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnBottomHint | Qt.WindowType.Tool)
+        self.setGeometry(self.x - (pixmap.width() + 50), self.y, pixmap.width() + 20, pixmap.height())
+
+if __name__ == "__main__":
+    comic_details = get_comic_details()
+    alt = comic_details.get("alt")
+    title = comic_details.get("safe_title")
+    url = comic_details.get("img")
+    get_comic(url, title)
+
+    app = QApplication(sys.argv)
+    screen = app.primaryScreen()
+    x = screen.size().width()
+    y = 30
+    window = MainWindow(x,y,alt,title)
+    window.show()
+
+    sys.exit(app.exec())
